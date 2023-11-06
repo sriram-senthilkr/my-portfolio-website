3dbfaa022433cc48d0a774fea960e9eb603b5447
Sriram <>
2023-11-07 01:21:44 +0800

    Added NavLinks to Navbar

---
+++ app/components/NavLink.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NavLink = ({ href, title }) => {
+    return (
+        <Link href={href} className="block py-2 pl-3 pr-4">
+            {title}
+        </Link>
+    );
+};
+
+export default NavLink;
